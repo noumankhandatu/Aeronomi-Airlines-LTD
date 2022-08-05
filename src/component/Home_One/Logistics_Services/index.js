87ecb7bf68800be6c1b7b53b780ac15342a10fb8
noumankhandatu <noumankhandatu>
2022-08-05 14:43:17 +0500

    logistic

--- src/component/Home_One/Logistics_Services/index.js
+++ src/component/Home_One/Logistics_Services/index.js
@@ -1,15 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // Import LogisticsCard
 import LogisticsCard from "./LogisticsCard";
-// Import LogisticsData
-import { LogisticsData } from "./Logistics_Data";
 // import Section Heading
 import SectionHeading from "../../Common/SectionHeading";
 //  OwlCarousel Slider Import
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
-
+import Prismic from "prismic-javascript";
+import api from "../../../prismicApi";
 const LogisticsService = () => {
   const [sliderIndex, setSliderIndex] = useState(0);
 
@@ -27,15 +26,135 @@ const LogisticsService = () => {
       items: 3,
     },
   };
+  const [toggleFn, setToggleFn] = useState(true);
+  const [fetchData, setFetchData] = useState("");
+  async function getServerSideProps() {
+    const client = Prismic.client(api);
+    client
+      .query([Prismic.Predicates.at("document.type", "home")])
+      .then((res) => {
+        setFetchData(res);
+      })
+      .catch((err) => {
+        console.log("err is ", err);
+      });
+  }
+  if (toggleFn) {
+    getServerSideProps();
+    setToggleFn(!toggleFn);
+  }
+  const title = fetchData?.results?.map((items) => {
+    return items.data.body[2].items[0].title;
+  });
+  const description = fetchData?.results?.map((items) => {
+    return items.data.body[2].items[0].description;
+  });
+
+  const itemCaroTextOne = fetchData?.results?.map((items) => {
+    return items.data.itemcarotextone;
+  });
+  const itemCaroTextTwo = fetchData?.results?.map((items) => {
+    return items.data.itemcarotexttwo;
+  });
+  const itemCaroTextThree = fetchData?.results?.map((items) => {
+    return items.data.itemcarotextthree;
+  });
+
+  const itemImageOne = fetchData?.results?.map((items) => {
+    return items.data.itemimageone.url;
+  });
+  const itemHeadingOne = fetchData?.results?.map((items) => {
+    return items.data.itemheadingone;
+  });
+  const itemParaOne = fetchData?.results?.map((items) => {
+    return items.data.itemparaone;
+  });
+
+  const itemImageTwo = fetchData?.results?.map((items) => {
+    return items.data.itemimagetwo.url;
+  });
+  const itemHeadingTwo = fetchData?.results?.map((items) => {
+    return items.data.itemheadingtwo;
+  });
+  const itemParaTwo = fetchData?.results?.map((items) => {
+    return items.data.itemparatwo;
+  });
+
+  const itemImageThree = fetchData?.results?.map((items) => {
+    return items.data.itemimagethree.url;
+  });
+  const itemHeadingThree = fetchData?.results?.map((items) => {
+    return items.data.itemheadingthree;
+  });
+  const itemParaThree = fetchData?.results?.map((items) => {
+    return items.data.itemparathree;
+  });
+
+  const LogisticsData = [
+    [
+      {
+        img: itemImageOne ? itemImageOne : `pending`,
+        heading: itemHeadingOne ? itemHeadingOne : `pending`,
+        para: itemParaOne ? itemParaOne : `pending`,
+      },
+      {
+        img: itemImageTwo ? itemImageTwo : `pending`,
+        heading: itemHeadingTwo ? itemHeadingTwo : `pending`,
+        para: itemParaTwo ? itemParaTwo : `pending`,
+      },
+      {
+        img: itemImageThree ? itemImageThree : `pending`,
+        heading: itemHeadingThree ? itemHeadingThree : `pending`,
+        para: itemParaThree ? itemParaThree : `pending`,
+      },
+    ],
+    [
+      {
+        img: itemImageOne ? itemImageOne : `pending`,
+        heading: itemHeadingOne ? itemHeadingOne : `pending`,
+        para: itemParaOne ? itemParaOne : `pending`,
+      },
+      {
+        img: itemImageTwo ? itemImageTwo : `pending`,
+        heading: itemHeadingTwo ? itemHeadingTwo : `pending`,
+        para: itemParaTwo ? itemParaTwo : `pending`,
+      },
+      {
+        img: itemImageThree ? itemImageThree : `pending`,
+        heading: itemHeadingThree ? itemHeadingThree : `pending`,
+        para: itemParaThree ? itemParaThree : `pending`,
+      },
+    ],
+    [
+      {
+        img: itemImageOne ? itemImageOne : `pending`,
+        heading: itemHeadingOne ? itemHeadingOne : `pending`,
+        para: itemParaOne ? itemParaOne : `pending`,
+      },
+      {
+        img: itemImageTwo ? itemImageTwo : `pending`,
+        heading: itemHeadingTwo ? itemHeadingTwo : `pending`,
+        para: itemParaTwo ? itemParaTwo : `pending`,
+      },
+      {
+        img: itemImageThree ? itemImageThree : `pending`,
+        heading: itemHeadingThree ? itemHeadingThree : `pending`,
+        para: itemParaThree ? itemParaThree : `pending`,
+      },
+    ],
+  ];
+
+  useEffect(() => {
+    getServerSideProps();
+  }, []);
 
   return (
     <>
       <section id="logistics_area">
         <div className="container">
           <SectionHeading
-            heading="Logistics Services"
-            para="Solving your supply chain needs from end to end, taking the
-             complexity out of container shipping. We are at the forefront of developing innovation."
+            heading={title ? title : `pending`}
+            para={description ? description : `pending`}
           />
           <div className="row">
             <div className="col-md-12">
@@ -47,7 +166,7 @@ const LogisticsService = () => {
                         className={sliderIndex === 0 ? "active" : ""}
                         onClick={() => setSliderIndex(0)}
                       >
-                        Business Logistics
+                        {itemCaroTextOne ? itemCaroTextOne : `pending`}
                       </button>
                     </li>
                     <li>
@@ -55,7 +174,7 @@ const LogisticsService = () => {
                         className={sliderIndex === 1 ? "active" : ""}
                         onClick={() => setSliderIndex(1)}
                       >
-                        Military Logistics
+                        {itemCaroTextTwo ? itemCaroTextTwo : `pending`}
                       </button>
                     </li>
                     <li>
@@ -63,7 +182,7 @@ const LogisticsService = () => {
                         className={sliderIndex === 2 ? "active" : ""}
                         onClick={() => setSliderIndex(2)}
                       >
-                        Event Logistics
+                        {itemCaroTextThree ? itemCaroTextThree : `pending`}
                       </button>
                     </li>
                   </ul>
