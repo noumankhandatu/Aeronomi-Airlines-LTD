0536978ec898fcac6cc35b466e066499819a56b2
noumankhandatu <noumankhandatu>
2022-08-16 15:22:51 +0500

    gallery page is dynamic

--- src/page/Gallery.js
+++ src/page/Gallery.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CommonBanner from "../component/Common/Banner";
 import GalleryImages from "../component/Gallery";
 import OurPartner from "../component/Common/OurPartner";
+import Prismic from "prismic-javascript";
+import api from "../prismicApi";
 
 const Gallery = () => {
+  const [toggleFn, setToggleFn] = useState(true);
+  const [fetchData, setFetchData] = useState("");
+  async function getServerSideProps() {
+    const client = Prismic.client(api);
+    client
+      .query([Prismic.Predicates.at("document.type", "gallery")])
+      .then((res) => {
+        setFetchData(res);
+      })
+      .catch((err) => {
+        console.log("err is ", err);
+      });
+  }
+  if (toggleFn) {
+    getServerSideProps();
+    setToggleFn(!toggleFn);
+  }
+  const bannerheading = fetchData?.results?.map((items) => {
+    return items.data.body[0].items[0].bannerheading;
+  });
+  const bannerpage = fetchData?.results?.map((items) => {
+    return items.data.body[0].items[0].bannerpage;
+  });
+  const bgimage = fetchData?.results?.map((items) => {
+    return items.data.body[0].items[0].bgimage.url;
+  });
+
+  useEffect(() => {
+    getServerSideProps();
+  }, []);
   return (
     <>
-      <CommonBanner heading="Gallery" page="Gallery" />
+      <CommonBanner
+        image={bgimage ? bgimage : `pending`}
+        heading={bannerheading ? bannerheading : `pending`}
+        page={bannerpage ? bannerpage : `pending`}
+      />
       <GalleryImages />
       <OurPartner />
     </>
